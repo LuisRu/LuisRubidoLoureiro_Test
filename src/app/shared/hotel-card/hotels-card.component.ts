21b6441e5ea327e98044e17a1c7670fec7ae0709
LuisRu <luisrubidoloureiro@hotmail.com>
2025-11-05 13:11:25 +0100

    Fix: imagen

--- src/app/shared/hotel-card/hotels-card.component.ts
+++ src/app/shared/hotel-card/hotels-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Hotel } from '../../core/models/hotel';
 import { CurrencyPipe, NgOptimizedImage } from '@angular/common';
 
@@ -6,7 +6,8 @@ import { CurrencyPipe, NgOptimizedImage } from '@angular/common';
   selector: 'app-hotels-card',
   standalone: true,
   imports: [NgOptimizedImage, CurrencyPipe],
-  templateUrl: './hotels-card.component.html'
+  templateUrl: './hotels-card.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HotelsCardComponent {
   hotel = input.required<Hotel>();
